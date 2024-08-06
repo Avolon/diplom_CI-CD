ff2096503e85f3d00ba5e9bbc81ce75497907cab
avolon <avolon.linux@gmail.com>
2024-08-06 10:29:54 +0300

    add

---
+++ test_deploy_app.sh
@@ -0,0 +1,5 @@
+docker compose --project-name test_project_app up -d
+
+#docker compose up -d --project-name test_project_app
+docker ps -a
+echo "Приложение django-pg-docker-tutorial для теста развёрнуто удачно!"
